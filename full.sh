094dc312bdd9354760350d7ef85e780feb1467ad
dmaiocchi <dmaiocchi@suse.com>
2019-01-08 19:16:36 +0100

    init

---
+++ full.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+kubeadm_dir="/home/dmaiocchi/bin/kubeadm-dev"
+kubeadm_playground="$GOPATH/src/k8s.io/kubeadm/vagrant/kubeadm-playground"
+
+# Deploy in multi-cluster setup
+export KUBEADM_PLAYGROUND_SPEC=$kubeadm_dir
+$kubeadm_playground start
+
+# deploy development k8s binary of kubeadm
+$kubeadm_playground deploy --binary ~/go/src/github.com/kubernetes/kubernetes_output/bin/kubeadm
